7c7e01406f84f5b8b261b7519663a3e9c5c4eb26
Maciej <maciej.polowy1@gmail.com>
2026-01-27 09:59:04 +0100

    refactor Stripe webhook handling to improve event validation and order payment processing

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -1,18 +1,54 @@
-import { markAsPaid } from "@/lib/actions/order";
+import { markOrderPaidInternal } from "@/lib/actions/order";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 export async function POST(request: NextRequest) {
-  const event = await Stripe.webhooks.constructEvent(
-    await request.text(),
-    request.headers.get("Stripe-Signature") as string,
-    process.env.STRIPE_WEBHOOK_SECRET as string,
-  );
+  const signature = request.headers.get("Stripe-Signature");
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  if (!signature || !webhookSecret) {
+    return NextResponse.json(
+      { error: "Missing signature or webhook secret" },
+      { status: 400 },
+    );
+  }
+
+  let event: Stripe.Event;
+  try {
+    event = Stripe.webhooks.constructEvent(
+      await request.text(),
+      signature,
+      webhookSecret,
+    );
+  } catch (error) {
+    console.error("Error constructing Stripe event:", error);
+    return NextResponse.json(
+      { error: "Invalid Stripe webhook signature" },
+      { status: 400 },
+    );
+  }
 
   if (event.type === "charge.succeeded") {
     const { object } = event.data;
-    await markAsPaid(object.metadata.order_id);
-    return NextResponse.json({ message: "markAsPaid was successfull" });
+
+    if (!object.metadata?.order_id) {
+      return NextResponse.json(
+        { error: "Missing order_id in metadata" },
+        { status: 400 },
+      );
+    }
+
+    try {
+      await markOrderPaidInternal(object.metadata.order_id);
+      return NextResponse.json({
+        message: "markOrderPaidInternal was successful",
+      });
+    } catch (error) {
+      console.error("Error marking order as paid:", error);
+      return NextResponse.json(
+        { error: "Failed to mark order as paid" },
+        { status: 500 },
+      );
+    }
   }
   return NextResponse.json({ message: "event is not charge.succeeded" });
 }
